5c245c37ee2b5d3ebf864e36d9cd37c8d0733791
Gaston <truffe_gaston@yahoo.com>
2023-02-28 01:06:33 -0300

    8-se agregaron mas funciones de js

--- js/aplicacion.js
+++ js/aplicacion.js
@@ -1,3 +1,4 @@
+// Calcula edad
 function getEdad() {
     let hoy = new Date()
     let fechaNacimiento = new Date("1989/10/02")
@@ -7,4 +8,23 @@ function getEdad() {
       edad--
     }
     document.getElementById('edad').innerHTML = edad;
-  }
+}
+
+// Modo Oscuro
+document.getElementById('btn_oscuro').onclick=function(){
+  document.body.style.backgroundColor = '#454545';
+  document.body.style.color = '#FFFFFF';
+}
+
+// Modo Claro
+document.getElementById('btn_claro').onclick=function(){
+  document.body.style.backgroundColor = '#FFFFFF';
+  document.body.style.color = '#000000';
+}
+
+
+// Boton cerrar
+document.getElementById("btn_salir").onclick = function(){
+  alert("Gracias por visitar esta pagina");
+  window.close();
+}
